303b33677c24b74813642850248796f410a9c2c8
AlexPeppas <apeppas@deloitte.gr>
2020-04-14 18:26:34 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const args = process.argv.slice(2);
+var flag = 0;
+console.log (args);
+if (args.length>10){
+	console.log("You've passed more than 10 arguements. Prohibited")
+	process.exit(1);
+}	else {
+	for (let i=0;i<args.length;i++){
+		if (parseInt(args[i])<0){
+			console.log("the number in position "+i+" of arguements is not positive. Prohibited");
+			flag = 1;
+		}
+	}
+	
+	if (flag)
+	{
+	//	process.exitCode = 1;
+	process.exit(1);
+	}
+}
+console.log ("valid input");
+/*const args = process.argv.slice(2);
+if (args.length !==1){
+    console.log('Please pass one single arguement');
+    process.exit(1);
+}   else if (parseInt(args[0])<=0){
+    console.log('please pass a positive arguement');
+    process.exit(1);
+}
+var input = args[0];
+Lastname(input);
+function Lastname (input){
+    if (input>=10){
+        console.log('Peppas');
+    }   else{ 
+        console.log('Kyriakos');
+    }
+}
+*/
